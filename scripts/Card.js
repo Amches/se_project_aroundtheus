602771fe10a5b9f837d5cc9e5d4f4ededf6173d0
Amches2 <dan8363@gmail.com>
2024-12-21 22:12:52 +0200

    project7 first debugs

---
+++ scripts/Card.js
@@ -0,0 +1,57 @@
+class Card {
+  constructor(data, cardSelector, handleCardImageClick) {
+    this._name = data.name;
+    this._link - data.link;
+    this._cardSelector = cardSelector;
+    this._handleCardImageClick = handleCardImageClick;
+  }
+  _setEventListeners() {
+    this._cardElement
+      .querySelector(".card__image")
+      .addEventListener("click", () => {
+        this._handleImageClick(this._name, this._link);
+      });
+
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeButton();
+      });
+
+    this._cardElement
+      .querySelector(".card__trash-button")
+      .addEventListener("click", () => {
+        this._handleDeleteButton();
+      });
+  }
+
+  _handleDeleteButton() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  _handleLikeButton() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_active");
+  }
+
+  getCardElement() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    this._cardImageElement = this._cardElement.querySelector(".card__image");
+    this._cardTitleElement = this._cardElement.querySelector(".card__title");
+    this._cardImageElement.src = this._link;
+    this._cardImageElement.alt = this._name;
+    this._cardTitleElement.textContent = this._name;
+    this._likeButton = this._cardElement.querySelector(".card__like-button");
+
+    this._setEventListeners();
+    return this._cardElement;
+  }
+}
+
+export default Card;
